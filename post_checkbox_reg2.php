5f54f365909ea6e7aedaa9895a8ec630cc60aac0
MasaharuIoki <m-ioki@bps21.jp>
2021-02-12 11:42:41 +0900

    update

---
+++ post_checkbox_reg2.php
@@ -0,0 +1,13 @@
+<!-- サンプルコード -->
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>post_reg2</title>
+</head>
+<body>
+ 
+<h1>POST受信</h1>
+<?php echo $_POST['sitename']; ?>
+ 
+</body>
+</html>
